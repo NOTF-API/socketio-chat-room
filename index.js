8c372945517548f0907c904f4b274a360e2e65f3
NOTF-API <1764914014@qq.com>
2021-04-18 11:04:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server,{
+  cors:"192.168.1.8:80"
+});
 
 app.use(express.static(__dirname + '/public'));
 let online=0;
